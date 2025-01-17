1a8bc6a1bd649ed26620e8df21dd45b429af00c9
vnord <ari@vnord.net>
2025-01-17 14:42:34 +0100

    attempt to fix gh-pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // other config options...
+  basePath: '/vnord.github.io',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
