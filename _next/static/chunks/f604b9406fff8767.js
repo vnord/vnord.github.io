511bc1ac253f73662ef3717eec38ccfd9ee7905e
vnord <vnord@users.noreply.github.com>
2025-11-30 17:48:09 +0000

    deploy: 1209d6a92715ee67c0a01d4476810d68aeea67ed

---
+++ _next/static/chunks/f604b9406fff8767.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,20199,c=>{c.v(t=>Promise.all(["static/chunks/0c77b9bd7c6ba032.js"].map(t=>c.l(t))).then(()=>t(56504)))}]);
